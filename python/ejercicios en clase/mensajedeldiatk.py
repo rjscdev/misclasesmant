6b0328c9f160ea4b3324789167dc876cb51f9ca5
Roberto <rjsc492004@gmail.com>
2024-08-01 13:02:22 -0600

    hola

--- python/ejercicios en clase/mensajedeldiatk.py
+++ python/ejercicios en clase/mensajedeldiatk.py
@@ -1,31 +1,27 @@
 import tkinter as tk
 import random
 
-# Lista de mensajes del día
-mensajes = {
-    "mensaje1": "Hoy es un buen día para aprender algo nuevo.",
-    "mensaje2": "La perseverancia es la clave del éxito.",
-    "mensaje3": "La creatividad es la inteligencia divirtiéndose.",
-    "mensaje4": "Cada día es una nueva oportunidad para ser mejor.",
-    "mensaje5": "La actitud positiva es contagiosa. Sé un portador."
+diccionario = {
+    'clave1' : "el aady",
+    'clave2' : "hola",
+    'clave3' : "la carne de chancho",
+    'clave4' :  "la vida es una lenteja o la tomas o la dejas"
 }
 
-# Función para mostrar un mensaje aleatorio
-def mostrar_mensaje():
-    mensaje = random.choice(list(mensajes.values()))
-    etiqueta.config(text=mensaje)
+def mostrarmensaje():
+    mensaje = random.choice(list(diccionario.values()))
+    label.config(text= mensaje)
+    
 
-# Configuración de la ventana principal de tkinter
-ventana = tk.Tk()
-ventana.title("Mensaje del Día")
 
-# Configuración de la etiqueta para mostrar el mensaje
-etiqueta = tk.Label(ventana, text="")
-etiqueta.pack(pady=20)
+root = tk.Tk()
+root.title("Mensaje diario")
+root.geometry("300x300")
 
-# Configuración del botón para generar un nuevo mensaje
-boton = tk.Button(ventana, text="Generar Mensaje", command=mostrar_mensaje)
-boton.pack(pady=20)
+label = tk.Label(root, text="")
+label.pack(pady= 5)
 
-# Inicio del bucle principal de tkinter
-ventana.mainloop()
+boton = tk.Button(root, text="generar mensaje", command= mostrarmensaje)
+boton.pack(pady= 5)
+
+root.mainloop()    
